2ce568e584b6111a387cbf7daf856f700ce669f1
Wilton Nunes Gomes da Silva <will_nunnes@hotmail.com>
2021-10-27 20:49:58 -0300

    Alterar aquivo main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 
 int btnCurrState = GLUT_UP, btnPrevState = GLUT_UP, x0, y0;
+double angle = 45, aspect;
 
 void init(void) {
 	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glFrustum(-1, 1, -1, 1, -1, 1);
+	aspect = 0;
 }
 
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 1.0);
+
+	glTranslatef(0, 0, 100);
 
 	glutWireCube(0.5);
 
@@ -34,6 +37,25 @@ void onMouse(int button, int state, int x, int y) {
 	}
 }
 
+void setDisplayParams() {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	gluPerspective(angle, aspect, 0.1, 500);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0, 0, 0, 0, 0, 100, 0, 0, 1);
+}
+
+void reshape(int width, int height) {
+	if(height == 0)
+		height = 1;
+
+	glViewport(0, 0, width, height);
+    aspect = (double)width / (float)height;
+    setDisplayParams();
+}
+
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
@@ -43,6 +65,7 @@ int main(int argc, char** argv) {
 	init();
 	glutDisplayFunc(display);
 	glutMouseFunc(onMouse);
+	glutReshapeFunc(reshape);
 	glutMainLoop();
 	return 0;
 }
